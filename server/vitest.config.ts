48050276bc3cec5100abb13e91c71633477a8a21
Sekkmer <sekkmer@gmail.com>
2025-08-26 22:57:35 +0200

    feat: improve tests

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -4,6 +4,15 @@ export default defineConfig({
 	test: {
 		environment: 'node',
 		include: ['tests/**/*.test.ts'],
+		pool: 'forks',
+		isolate: true, 
+		slowTestThreshold: 1_000,
+		testTimeout: 2_000,
+		hookTimeout: 10_000,
+		teardownTimeout: 10_000,
+		poolOptions: {
+			forks: { singleFork: false }
+		},
 		coverage: {
 			provider: 'v8',
 			reportsDirectory: './coverage',
